6c1ccd0ddd4b41bacfabfb550f03246c0234849d
Todd Sutherland <sutherlandwebdev@gmail.com>
2022-08-02 12:45:30 -0500

    event listener on click, use method play()

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ sounds.forEach((sound) => {
   const btn = document.createElement("button"); // create a button element for each sound
   btn.classList.add("btn"); // give each button a class
 
-  btn.innerText = sound; // set innerText to the sound itself
+  btn.innerText = sound; // set innerText to the sound
+
+  // for every button, we want to listen for a click. On that click, we want to get the button element by ID, and use audio element method, play()
+  btn.addEventListener("click", () => {
+    document.getElementById(sound).play();
+  });
 
   document.getElementById("buttons").appendChild(btn); // get the buttons container and append each btn to it
 });
